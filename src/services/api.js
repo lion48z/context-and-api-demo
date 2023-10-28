fd5d2e71dd61191ffeb53cf80212b36c348118d7
lion48z <140863653+lion48z@users.noreply.github.com>
2023-10-28 16:47:29 -0500

    added env
    demonstrate how to hide API KEY and ID

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = 'https://randomuser.me/api/'
+
+//const API_KEY = ''
+// use secret /private environment variables 
+API_KEY = process.env.REACT_APP_API_KEY;
+
+const fetchRandomUser = asyn () => {
+    try{
+        //anything in here will be ran and if there is error it will run the code in the catch block instead of stopping
+        const response = await axios.get(BASE_URL);
+        return response.data.results [0]
+    } catch (error) {
+       console.error("failed to fetch user", error); //console.error is specific way to log out errors
+    }
+}
